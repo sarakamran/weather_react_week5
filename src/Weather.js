efcccf8d899e3363f1693adda60b767a054737b6
Sara Kamran <sara.kamran72@gmail.com>
2023-06-04 14:04:50 +0330

    icon and units

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/48/sunny.png",
+      icon: response.data.weather[0].icon,
     });
   }
   function search() {
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,8 +1,10 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-    console.log(props.data.city);
+  console.log(props.data.city);
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
@@ -10,43 +12,22 @@ export default function WeatherInfo(props) {
         <li>
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">
-          {props.data.description}
-        </li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row">
         <div className="col-6">
           <div className="clearfix weather-temperature">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-              id="icon"
-            />
+            <WeatherIcon code={props.data.icon} size={52} />
+
             <span className="float-left">
-              <strong id="temperature">
-                {props.data.temperature}
-              </strong>
-              <span className="units">
-                <small>
-                  {" "}
-                  <a href="#" id="celsius-link" className="active">
-                    °C
-                  </a>
-                  |{" "}
-                  <a href="#" id="fahrenheit-link">
-                    °F
-                  </a>
-                </small>
-              </span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </span>
           </div>
         </div>
         <div className="col-6">
           <ul>
             <li>
-              Humidity:{" "}
-              <span id="humidity">{props.data.humidity}</span> %
+              Humidity: <span id="humidity">{props.data.humidity}</span> %
             </li>
             <li className="lowerize">
               Wind: <span id="wind">{props.data.wind}</span> km/h
